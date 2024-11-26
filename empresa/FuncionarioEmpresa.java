9e03523293f919e1be4f959a1c40a5fed64d0695
Samuel Victor <samuelvictor0013@gmail.com>
2024-11-26 01:05:05 -0300

    Create FuncionarioEmpresa.java

---
+++ empresa/FuncionarioEmpresa.java
@@ -0,0 +1,16 @@
+package empresa;
+
+public class FuncionarioEmpresa {
+    private String nome;
+    private double salario;
+
+    public FuncionarioEmpresa (String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário: " + salario);
+    }
+}
