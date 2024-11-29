ed13a4f80fd273b07ec77494f24d8754477b638e
Samuel Victor <samuelvictor0013@gmail.com>
2024-11-29 00:50:37 -0300

    Update setorFiscal.java

--- setor/setorFiscal.java
+++ setor/setorFiscal.java
@@ -4,16 +4,25 @@ package setor;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class setorFiscal extends setorPai {
+
+public class setorFiscal extends setorPai implements data{
  
     Scanner recebe = new Scanner(System.in);
+
+    public void realizarAtividades() {
+        super.realizarAtividades();
+    }
+
+    void data(){
+        System.out.println("O prazo de entrega é até o dia 15");
+    }
 
     public setorFiscal() {
         super("Setor Fiscal");
     }
 
     public void calcularFaturamento() {
-        double[] faturamento = new double[12]; // Array para os 12 meses
+        double[] faturamento = new double[11]; // Array para os 12 meses
         String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
         Scanner recebe = new Scanner(System.in);
